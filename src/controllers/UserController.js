a7c19f5667ab46d0dab586c2d496af03ae29a355
Lucas Gabriel <lucas.g4briel.04@gmail.com>
2023-04-20 13:17:13 -0300

    Feature: CRUD User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,86 @@
 import User from '../models/User';
 
 class UserController {
+  // CREATE
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // INDEX
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // SHOW
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // UPDATE
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(401).json({
+          errors: ['ID not send'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(401).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const newUser = await user.update(req.body);
+      return res.json(newUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // DELETE
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID not send'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(401).json({
+          errors: ['User not found'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
