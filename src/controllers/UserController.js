0eb335879f63a27822e8ebb0b9fdbfd4f0101121
Lucas Gabriel <lucas.g4briel.04@gmail.com>
2023-04-25 15:20:17 -0300

    Feature: Middleware LoginRequired Added

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -17,6 +17,8 @@ class UserController {
   async index(req, res) {
     try {
       const users = await User.findAll();
+      console.log(req.userId);
+      console.log(req.userEmail);
       return res.json(users);
     } catch (e) {
       return res.json(null);
--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,3 +1,25 @@
+import jwt from 'jsonwebtoken';
+
 export default (req, res, next) => {
-  next();
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(403).json({
+      errors: ['Login required'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  } catch (e) {
+    return res.status(403).json({
+      errors: ['Expired Token or Invalid Token'],
+    });
+  }
 };
