6d8f0568c1f781d0cb6ac4c0eadd0e9d45f2144a
Lucas Gabriel <lucas.g4briel.04@gmail.com>
2023-05-16 11:12:27 -0300

    Adicionando CORS

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
 import fotoRoutes from './src/routes/fotoRoutes';
+
+const whitelist = [
+  'http://localhost:3000',
+];
+
+const corsOtions = {
+  origin(origin, callback) {
+    if (!whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -21,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOtions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -30,7 +30,7 @@ class TokenController {
       expiresIn: process.env.TOKEN_TTL,
     });
 
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
